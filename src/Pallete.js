df1b78ac219bde1a664110b7cab0d93d791337f7
Ihor <igortkachuk0426@gmail.com>
2020-02-06 15:36:06 +0200

    Added Pallete component and basic color palletes

---
+++ src/Pallete.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Pallete extends Component {
+    render() {
+        return (
+            <div className="Pallete">
+                {/* Navbar */}
+                <div className="Pallete-colors">
+                    {/* Colorboxes */}
+                </div>
+                {/* footer */}
+            </div>
+        )
+    }
+}
+
+export default Pallete

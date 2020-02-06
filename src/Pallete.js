6884946804f414a9174fa381016affefabff5676
Ihor <igortkachuk0426@gmail.com>
2020-02-06 15:49:20 +0200

    ColorBox component created and basic styles for distinction

--- src/Pallete.js
+++ src/Pallete.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
-
+import ColorBox from './ColorBox'
+import './Pallete.css'
 class Pallete extends Component {
     render() {
+        const colorBoxes = this.props.colors.map((color, index) => (
+            <ColorBox key={index} background={color.color} name={color.name} />
+        ))
         return (
             <div className="Pallete">
                 {/* Navbar */}
                 <div className="Pallete-colors">
-                    {/* Colorboxes */}
+                    {colorBoxes}
                 </div>
                 {/* footer */}
             </div>

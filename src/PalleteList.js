7928a183a98e12f0894c62d4420879ac553ce5f6
Ihor <igortkachuk0426@gmail.com>
2020-02-10 15:08:56 +0200

    Added materialUI styles and MiniPallete component

--- src/PalleteList.js
+++ src/PalleteList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
+import MiniPallete from './MiniPallete';
 class PalleteList extends Component {
     render() {
         const { palletes } = this.props;
         return (
             <div>
+                <MiniPallete />
                 {palletes.map(p => (
                     <Link to={`/palette/${p.id}`}>{p.paletteName}</Link>
                 ))}
